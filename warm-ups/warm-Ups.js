d8543aadd5a1eb1e2a1a5387c21f08a92e180817
Lance Burk <burk.lance.b@gmail.com>
2023-10-12 11:04:25 -0500

    completed exercise 1-4

--- warm-ups/warm-Ups.js
+++ warm-ups/warm-Ups.js
@@ -2,4 +2,4 @@
 //Daily Warm-ups
 let name = "Lance Burk"
 let year = "2016"
-console.log('My name is ' + name + 'and i graduated high school in ' + year)
+console.log('My name is ' + name + ' and I graduated high school in ' + year)
