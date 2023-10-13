ea2b60dbc071d049558eec6554deb91abaf86fc0
Lance Burk <burk.lance.b@gmail.com>
2023-10-13 09:20:29 -0500

    daily warm up Oct 13/ helloworld.html file linked to helloworld.js in js warmups folder

---
+++ js/javascript-with-html.js
@@ -0,0 +1,19 @@
+"use strict"
+
+let myAge = 5
+console.log(myAge)
+
+//alert
+alert("Coding is FUN!")
+
+let confirmed = confirm("Are you sure you're in the correct cohort?")
+console.log("this is my external js file")
+
+console.log(confirmed)
+
+//prompt
+let userInput = prompt("Please type in your name")
+alert(`${userInput}`)
+
+
+console.log("This is my external js file")
---
+++ warm-ups/js/helloWorld.js
@@ -0,0 +1,2 @@
+userAge = prompt (`What is your age?`)
+alert = `Wow, ${userAge}? your'e old!`
