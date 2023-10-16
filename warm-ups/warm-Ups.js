18c3ae0b33af78495e4e448ba7bc1020c1470de2
Lance Burk <burk.lance.b@gmail.com>
2023-10-16 09:30:58 -0500

    Daily Warm-ups Oct 16, 2023

--- warm-ups/warm-Ups.js
+++ warm-ups/warm-Ups.js
@@ -1,5 +1,11 @@
 // Lance Burk
 //Daily Warm-ups
-let name = "Lance Burk"
+/*let name = "Lance Burk"
 let year = "2016"
 console.log('My name is ' + name + ' and I graduated high school in ' + year)
+
+************/
+
+//Daily Warm-ups Oct, 16, 2023
+const capitalizedFirstLetter = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+console.log(capitalizedFirstLetter("wow"))
