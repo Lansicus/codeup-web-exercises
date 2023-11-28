598d34f27594ecfcd9c79fe2d391844157a26f0d
Lance Burk <burk.lance.b@gmail.com>
2023-11-28 09:03:51 -0600

    finished first portion handle an API

--- assignments/Nov-2023/Handle_API_Promise/fetch.js
+++ assignments/Nov-2023/Handle_API_Promise/fetch.js
@@ -1,38 +1,72 @@
 /*GO HERE FOR MORE TOKENS https://github.com/settings/tokens/new
 THIS TOKEN WILL NOT WORK AFTER DEC, 27, 2023*/
 
-// fetch(url, {headers: {'Authorization': 'token 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'}})
-//     .then(r => )
 
-function getLastCommitDate(username, token) {
-    const url = `https://api.github.com/users/${username}/received_events/public`;
+/*Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of
+ the last commit that user made. Reference the GitHub api documentation to achieve this.*/
+function getLastCommitDate(username) {
+    // GitHub API URL for user events
+    const GITHUB_API_URL = `https://api.github.com/users/${username}/events`;
 
-    return fetch(url, {
-        headers: {
-            'Accept': 'application/vnd.github+json',
-            'Authorization': `Bearer ${token}`,
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`GitHub API error: ${response.status} - ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(events => {
-            const commitEvents = events.filter(event => event.type === 'PushEvent');
-            if (commitEvents.length > 0) {
-                return commitEvents[0].created_at;
-            } else {
-                throw new Error('No commit events found.');
-            }
-        });
+    // Return a new Promise
+    return new Promise((resolve, reject) => {
+        // Fetch data from the GitHub API
+        fetch(GITHUB_API_URL)
+            .then(response => {
+                // Check if the response is successful
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch data. Status: ${response.status}`);
+                }
+                // Parse the response as JSON
+                return response.json();
+            })
+            .then(events => {
+                // Filter events to only include PushEvents
+                const pushEvents = events.filter(event => event.type === 'PushEvent');
+
+                // Check if there are any push events
+                if (pushEvents.length > 0) {
+                    // Get the date of the first push event
+                    const lastCommitDate = new Date(pushEvents[0].created_at);
+                    // Resolve the promise with the formatted date string
+                    resolve(lastCommitDate.toDateString());
+                } else {
+                    // Reject the promise with an error if no push events are found
+                    reject(new Error('No push events found for the specified user.'));
+                }
+            })
+            .catch(error => {
+                // Reject the promise with an error if there's any issue during the process
+                reject(new Error(`Error fetching data: ${error.message}`));
+            });
+    });
 }
 
-const username = prompt(`What is your-github-username?`);
-const token = prompt('What is your-personal-access-token?');
 
-getLastCommitDate(username, token)
-    .then(date => console.log(`The last commit was made on: ${date}`))
-    .catch(error => console.error(error.message));
+getLastCommitDate(GITHUB_USERNAME)
+    .then(lastCommitDate => {
+        console.log(`Last commit date for ${GITHUB_USERNAME}: ${lastCommitDate}`);
+    })
+    .catch(error => {
+        console.error(error.message);
+    });
+
+
+
+
+
+
+
+/*---------------------------------------------------------------------------------------------------- EXAMPLE -------*/
+// /*make an Ajax call to return the height and weight of rattata*/
+// fetch("https://pokeapi.co/api/v2/pokemon/rattata")
+//     .then(response => {
+//         if (!response.ok) {
+//             console.log("The request did not succeed!");
+//         }
+//         return response.json();
+//     })
+//     .then(data =>
+//         console.log(`This height is: ${data.height}\nThis weight is: ${data.weight}`))
+//     .catch(err => console.log(err.message))
+//
