29b0117b473813578b8f7e6f4e3c64209319e0ef
Lance Burk <burk.lance.b@gmail.com>
2023-11-30 13:11:19 -0600

    dun did a lot i tell u whut

---
+++ assignments/Nov-2023/weather_map/weather-icons.js
@@ -0,0 +1,13 @@
+
+
+const cloud = "<i class=\"fa-solid fa-cloud\"></i>"
+
+const cloudSunRain = "<i class=\"fa-solid fa-cloud-sun-rain\"></i>"
+
+const cloudSun = "<i class=\"fa-solid fa-cloud-sun\"></i>"
+
+const cloudRain = "<i class=\"fa-solid fa-cloud-rain\"></i>"
+
+const cloudHeavyRain = "<i class=\"fa-solid fa-cloud-showers-heavy\"></i>"
+
+const cloudThunder = "<i class=\"fa-solid fa-cloud-bolt\"></i>"
