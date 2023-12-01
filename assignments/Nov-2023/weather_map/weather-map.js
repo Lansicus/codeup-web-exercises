e9312516136822b9dfca6174efe0b4d81f6747c1
Lance Burk <burk.lance.b@gmail.com>
2023-11-30 22:56:01 -0600

    way too much

--- assignments/Nov-2023/weather_map/weather-map.js
+++ assignments/Nov-2023/weather_map/weather-map.js
@@ -1,6 +1,16 @@
+// weather-map.js
 
+/*-------------------------------------------------------------------------- ESTABLISH MARKER & POPUP FUNCTION -------*/
+function placeMarkerAndPopup(info, token, map) {
+    geocode(info.address, token).then(coords => {
+        console.log(coords);
+        let popup = new mapboxgl.Popup().setHTML(info.popupHTML);
+        let marker = new mapboxgl.Marker().setLngLat(coords).addTo(map).setPopup(popup);
+        popup.addTo(map);
+    });
+}
 
-/*------------------------------------------------------------------------------------------------ geocode -------*/
+/*--------------------------------------------------------------------------------- ESTABLISH GEOCODE FUNCTION -------*/
 function geocode(search, token) {
     // api url
     let baseUrl = 'https://api.mapbox.com';
@@ -8,92 +18,155 @@ function geocode(search, token) {
     let endPoint = '/geocoding/v5/mapbox.places/';
     // this fetch request takes in a url endpoint that will return the lgn, lat
     return fetch(`${baseUrl}${endPoint}${encodeURIComponent(search)}.json?access_token=${token}`)
-        .then( res => res.json() )
+        .then(res => res.json())
         // to get all the data from the request, comment out the following three lines...
-        .then( data => {
+        .then(data => {
             // console.log(data)
-            return data.features[0].center
+            return data.features[0].center;
         });
 }
 
-
-/*----------------------------------------------------------------------------------------- reverseGeocode -------*/
+/*------------------------------------------------------------------------- ESTABLISH REVERSE GEOCODE FUNCTION -------*/
 function reverseGeocode(coordinates, token) {
     let baseUrl = 'https://api.mapbox.com';
     let endPoint = '/geocoding/v5/mapbox.places/';
     return fetch(`${baseUrl}${endPoint}${coordinates.lng},${coordinates.lat}.json?access_token=${token}`)
-        .then( res => res.json() )
+        .then(res => res.json())
         // to get all the data from the request, comment out the following three lines...
-        .then( data => data.features[0].place_name );
+        .then(data => data.features[0].place_name);
 }
 
-
-/*---------------------------------------------------------------------------------------------- MAP STUFF -------*/
+/*--------------------------------------------------------------------------------------- ESTABLISHING THE MAP -------*/
 mapboxgl.accessToken = MAPKEY;
-// creating a new map
 const map = new mapboxgl.Map({
-    // setting the location for my map depending on the id in my html
-    container: 'map', // container ID
-    // this is setting my mapboxgl styling
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    // this decides how zoomed in my map is
-    zoom: 10, // starting zoom
-    // this add the center to my map using the lng, lat
-    center: [-98.4916, 29.4252] // [lng, lat]
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v11',
+    zoom: 1,
+    center: [-96.80330059331894, 32.777946644194934]
 });
-// setting a marker object
-const marker = new mapboxgl.Marker()
-    // adding marker to specific [lng, lat]
-    .setLngLat([-96.80330059331894, 32.777946644194934])
-    // adding marker to map
-    .addTo(map);
-// setting a popup object
-let popup = new mapboxgl.Popup()
-    // adding popup to specific [lng, lat]
-    .setLngLat([-96.80330059331894, 32.777946644194934])
-    // setting the content of my popup
-    .setHTML("<p>Codeup Rocks!</p>")
-    // set the maximum width of the popup
-    .setMaxWidth("300px")
-    // adds the popup to my map
-    .addTo(map);
-const codeupPopup = new mapboxgl.Popup()
-    .setHTML("<p>Welcome to San Antonio!</p>");
-marker.setPopup(codeupPopup);
-geocode("San Antonio", MAPKEY)
-    .then(result => { console.log(result)
-        map.setCenter(result);
-        map.setZoom(10)})
-// reverse geocode method from mapbox-geocoder-utils.js
-reverseGeocode({lng: -98.4861, lat: 29.4260}, MAPKEY).
-then( results => console.log(results) );
-const alamoInfo = {
-    address: "The Dallas World Aquarium, Dallas",
-    popupHTML: "<p>Welcome to the Alamo!</p>"
-};
-function placeMarkerAndPopup(info, token, map) {
-    geocode(info.address, token).then( coords => {
-        console.log(coords)
-        let popup = new mapboxgl.Popup()
-            .setHTML(info.popupHTML);
-        let marker = new mapboxgl.Marker()
-            .setLngLat(coords)
-            .addTo(map)
-            .setPopup(popup);
-        popup.addTo(map);
-    });
+
+/*-------------------------------------------------------------------------------------------- DRAGGABLE MARKER ------*/
+let marker = createMarker([-96.80330059331894, 32.777946644194934]);
+
+function createMarker(lngLat) {
+    const newMarker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat(lngLat)
+        .addTo(map);
+
+    newMarker.on('dragend', onDragEnd);
+
+    return newMarker;
 }
 
-placeMarkerAndPopup(alamoInfo, MAPKEY, map);
+function updateMarkerLocation(lngLat) {
+    marker.setLngLat(lngLat);
+}
 
+// Trigger a custom event when the location changes
+function triggerLocationChangeEvent(location) {
+    const event = new CustomEvent("locationChange", { detail: location });
+    document.dispatchEvent(event);
+}
 
-//     Search Functionality
-document.getElementById("sub").addEventListener("click", function() {
+// Weather data fetching function
+function fetchWeatherData(lng, lat) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?` +
+        `lat=${lat}&lon=${lng}` +
+        `&appid=${WEATHERKEY}` + `&units=imperial`)
+        .then(data => data.json())
+        .then(result => {
+            triggerLocationChangeEvent([lng, lat]); // Trigger locationChange event with the updated location
+        });
+}
+
+// Event listener for the submit button
+document.getElementById("sub").addEventListener("click", function () {
     let currentLocation = geocode(document.getElementById("search").value, MAPKEY);
 
     currentLocation.then(result => {
-        console.log(result)
-        map.setCenter([result[0], result[1]])
-    })
+        map.setCenter([result[0], result[1]]);
+        updateMarkerLocation(result, true); // Set fromSubmit to true
+        fetchWeatherData(result[0], result[1]); // Fetch weather data based on the new location
+    });
+});
 
-})
+// Event listener for the marker dragend
+marker.on('dragend', onDragEnd);
+
+function onDragEnd() {
+    const lngLat = marker.getLngLat();
+    updateMarkerLocation(lngLat);
+
+    coordinates.style.display = 'block';
+    coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
+
+    // move map to where the marker is dragged
+    map.flyTo({
+        zoom: 15,
+        center: [lngLat.lng, lngLat.lat],
+        essential: false
+    });
+
+    fetchWeatherData(lngLat.lng, lngLat.lat); // Fetch weather data based on the new marker location
+}
+
+/*------------------------------------------------------------------------------------ SEARCH BAR FUNCTIONALITY ------*/
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.getElementById("search");
+
+searchForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevents the default form submission behavior
+
+    let currentLocation = geocode(searchInput.value, MAPKEY);
+
+    currentLocation.then(result => {
+        // Set SameSite attribute for the cookie
+        document.cookie = "yourCookieName=yourCookieValue; samesite=None; secure";
+
+        map.setCenter([result[0], result[1]]);
+        updateMarkerLocation(result, true); // Set fromSubmit to true
+
+        // Trigger an event to notify weather-output.js about the location update
+        const locationChangeEvent = new CustomEvent("locationChange", { detail: result });
+        document.dispatchEvent(locationChangeEvent);
+    });
+});
+
+
+
+/*--------------------------------------------------------------------------------------------  ------*/
+
+
+//// setting a marker object
+// const marker = new mapboxgl.Marker()
+//     // adding marker to specific [lng, lat]
+//     .setLngLat([-96.80330059331894, 32.777946644194934])
+//     // adding marker to map
+//     .addTo(map);
+//// setting a popup object
+// let popup = new mapboxgl.Popup()
+//     // adding popup to specific [lng, lat]
+//     .setLngLat([-96.80330059331894, 32.777946644194934])
+//     // setting the content of my popup
+//     .setHTML("<p>Codeup Rocks!</p>")
+//     // set the maximum width of the popup
+//     .setMaxWidth("300px")
+//     // adds the popup to my map
+//     .addTo(map);
+// const codeupPopup = new mapboxgl.Popup()
+//     .setHTML("<p>Welcome to San Antonio!</p>");
+// marker.setPopup(codeupPopup);
+// geocode("San Antonio", MAPKEY)
+//     .then(result => { console.log(result)
+//         map.setCenter(result);
+//         map.setZoom(10)})
+//// reverse geocode method from mapbox-geocoder-utils.js
+// reverseGeocode({lng: -98.4861, lat: 29.4260}, MAPKEY).
+// then( results => console.log(results) );
+
+
+
+
+
--- assignments/Nov-2023/weather_map/weather-output.js
+++ assignments/Nov-2023/weather_map/weather-output.js
@@ -1,89 +1,90 @@
+// weather-output.js
 
+const cardDeck = document.getElementById("cardHolder");
 
+function fetchWeatherData(lng, lat) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?` +
+        `lat=${lat}&lon=${lng}` +
+        `&appid=${WEATHERKEY}` + `&units=imperial`)
+        .then(data => data.json())
+        .then(result => {
+            // Clear existing cards
+            cardDeck.innerHTML = '';
 
-const cardDeck = document.getElementById("cardHolder")
+            const day = result.list;
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?` +
-    'id=4726206' +
-    `&appid=${WEATHERKEY}` + `&units=imperial`)
-    .then(data => data.json())
-    .then(result => {
-        // console.log(result)
+            for (let i = 0; i < day.length; i += 8) {
+                const weather = day[i];
 
-        const day = result.list;
-        console.log(day)
-        // const icon = result.weather;
+                const date = new Date(weather.dt * 1000).toLocaleDateString();
+                const dayOfWeek = new Date(weather.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' });
 
-        for (let i = 0; i < day.length; i += 8) {
-            const weather = day[i]
+                const cardDiv = document.createElement("div");
+                cardDiv.classList.add("card");
+                const centerDiv = document.createElement("div");
+                centerDiv.classList.add("centered");
+                const dateDiv = document.createElement("div");
+                const spanHolder = document.createElement("div");
+                const tempHighDiv = document.createElement("div");
+                const tempLowDiv = document.createElement("div");
+                const iconDiv = document.createElement("div");
+                const descriptionDiv = document.createElement("div");
+                const humidityDiv = document.createElement("div");
+                const windDiv = document.createElement("div");
+                const pressureDiv = document.createElement("div");
+                const hr1 = document.createElement("hr");
+                const hr2 = document.createElement("hr");
+                const hr3 = document.createElement("hr");
+                const lineBreak = document.createElement("br");
+                const lineBreak1 = document.createElement("br");
+                const lineBreak2 = document.createElement("br");
+                const lineBreak3 = document.createElement("br");
+                const lineBreak4 = document.createElement("br");
+                const icon = document.createElement("img");
 
-            const date = new Date(weather.dt * 1000).toLocaleDateString();
-            const dayOfWeek = new Date(weather.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' });
+                icon.src = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+                dateDiv.innerText = `${dayOfWeek}: ${date}`;
+                tempHighDiv.innerText = `High: ${weather.main.temp_max}°F `;
+                tempLowDiv.innerText = `Low: ${weather.main.temp_min}°F`;
+                descriptionDiv.innerText = `Description: ${weather.weather[0].description}`;
+                humidityDiv.innerText = `Humidity: ${weather.main.humidity}%`;
+                windDiv.innerText = `Wind Speed: ${weather.wind.speed}`;
+                pressureDiv.innerText = `Pressure: ${weather.main.pressure}atm`;
 
-            const cardDiv = document.createElement("div")
-            cardDiv.classList.add("card")
-            const centerDiv = document.createElement("div")
-            centerDiv.classList.add("centered")
-            const dateDiv = document.createElement("div")
-            const spanHolder = document.createElement("div")
-            const tempHighDiv = document.createElement("div")
-            const tempLowDiv = document.createElement("div")
-            const iconDiv = document.createElement("div")
-            const descriptionDiv = document.createElement("div")
-            const humidityDiv = document.createElement("div")
-            const windDiv = document.createElement("div")
-            const pressureDiv = document.createElement("div")
-            // const cloudIcon = document.createElement("")
-            const hr1 = document.createElement("hr")
-            const hr2 = document.createElement("hr")
-            const hr3 = document.createElement("hr")
-            const lineBreak = document.createElement("br")
-            const lineBreak1 = document.createElement("br")
-            const lineBreak2 = document.createElement("br")
-            const lineBreak3 = document.createElement("br")
-            const lineBreak4 = document.createElement("br")
-            const icon = document.createElement("img")
+                cardDeck.appendChild(cardDiv);
 
+                cardDiv.appendChild(centerDiv);
+                centerDiv.appendChild(lineBreak);
+                centerDiv.appendChild(dateDiv);
+                centerDiv.appendChild(lineBreak1);
 
-            icon.src = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"
-            dateDiv.innerText = `${dayOfWeek}: ${date}`;
-            tempHighDiv.innerText = `High: ${weather.main.temp_max}°F `;
-            tempLowDiv.innerText = `Low: ${weather.main.temp_min}°F`;
-            descriptionDiv.innerText = `Description: ${weather.weather[0].description}`;
-            humidityDiv.innerText = `Humidity: ${weather.main.humidity}%`;
-            windDiv.innerText = `Wind Speed: ${weather.wind.speed}`;
-            pressureDiv.innerText = `Pressure: ${weather.main.pressure}atm`;
+                centerDiv.appendChild(tempHighDiv);
+                centerDiv.appendChild(tempLowDiv);
+                centerDiv.appendChild(lineBreak2);
 
-console.log(tempLowDiv)
-            cardDeck.appendChild(cardDiv)/*    <div class="card">*/
+                centerDiv.appendChild(icon);
 
-            cardDiv.appendChild(centerDiv)
-            centerDiv.appendChild(lineBreak)
-            centerDiv.appendChild(dateDiv)
-            centerDiv.appendChild(lineBreak1)
+                cardDiv.appendChild(lineBreak3);
 
-            centerDiv.appendChild(tempHighDiv)
-            centerDiv.appendChild(tempLowDiv)
-            centerDiv.appendChild(lineBreak2)
+                cardDiv.appendChild(descriptionDiv);
+                cardDiv.appendChild(hr1);
+                cardDiv.appendChild(humidityDiv);
 
-            centerDiv.appendChild(icon)
+                cardDiv.appendChild(hr2);
 
-            // cardDiv.appendChild(hr1)
-            cardDiv.appendChild(lineBreak3)
+                cardDiv.appendChild(windDiv);
 
-            cardDiv.appendChild(descriptionDiv)
-            cardDiv.appendChild(hr1)
-            cardDiv.appendChild(humidityDiv)
+                cardDiv.appendChild(hr3);
 
-            cardDiv.appendChild(hr2)
+                cardDiv.appendChild(pressureDiv);
+                cardDiv.appendChild(lineBreak4);
+            }
+        });
+}
 
-            cardDiv.appendChild(windDiv)
-
-            cardDiv.appendChild(hr3)
-
-            cardDiv.appendChild(pressureDiv)
-            cardDiv.appendChild(lineBreak4)
-
-        }
-
-    })
+// Listen for the locationChange event triggered by weather-map.js
+document.addEventListener("locationChange", function (event) {
+    const newLocation = event.detail;
+    // Fetch new weather data based on the updated location
+    fetchWeatherData(newLocation[0], newLocation[1]);
+});
